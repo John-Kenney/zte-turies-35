8d9e0d28132405057a029d80f34052f467654a6c
John Kenney <johnkenney02@gmail.com>
2012-03-12 05:12:58 +0000

    AXI oc fix?? I'm not sure if this code was limiting the axi speed or not...

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -781,16 +781,16 @@ static void __init acpu_freq_tbl_fixup(void)
 
 		if (pll0_needs_fixup && t->pll == ACPU_PLL_0)
 			SLOWER_BY(t->a11clk_src_div, 2);
-		if (axi_160mhz && drv_state.max_axi_khz >= 160000
+/*		if (axi_160mhz && drv_state.max_axi_khz >= 160000
 		    && t->ahbclk_khz > 128000)
 			t->axiclk_khz = 160000;
 		if (axi_200mhz && drv_state.max_axi_khz >= 200000
 		    && t->ahbclk_khz > 160000)
-			t->axiclk_khz = 200000;
+			t->axiclk_khz = 200000; */
 	}
 
 	t--;
-	drv_state.max_axi_khz = t->axiclk_khz;
+//	drv_state.max_axi_khz = t->axiclk_khz;
 
 	/* The default 7x27 ACPU clock plan supports running the AXI bus at
 	 * 200 MHz. So we don't classify it as Turbo mode.
